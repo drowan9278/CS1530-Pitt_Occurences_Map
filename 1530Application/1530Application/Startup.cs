afcd88f48c81def076aa7a8d63a20bc10b91b59a
drowan9278 <drowan9278@students.mc3.edu>
2018-09-04 19:25:44 -0400

    Basic Start to Project

---
+++ 1530Application/1530Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1530Application.Startup))]
+namespace _1530Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
