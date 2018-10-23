f8d82cf8940aeb4117b6469b8416d3c24103cf07
BrandonDonahue96 <bad68@pitt.edu>
2018-10-23 16:13:13 -0400

    Add files via upload

--- 1530Application/1530Application/Controllers/HomeController.cs
+++ 1530Application/1530Application/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace _1530Application.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "What is Oakland Inconvenience Tracker?";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us";
 
             return View();
         }
