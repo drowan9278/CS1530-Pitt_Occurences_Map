b7c005c64624ea4a4800b66562cd3656355e2dc4
drowan9278 <drowan9278@students.mc3.edu>
2018-10-24 14:31:27 -0400

    Created Sql Connection

--- 1530Application/1530Application/DbContext/OciContext.cs
+++ 1530Application/1530Application/DbContext/OciContext.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
 namespace _1530Application
 {
-    public class OciContext : DbContext
+    public class DbConnection
     {
-        public OciContext()
+        SqlConnection dbConnection;
+        public DbConnection()
         {
-
+            dbConnection = new SqlConnection("user id=admin;" +
+                                       "password=oakland1530;server=oitdb.ccubo8pyjzvy.us-east-1.rds.amazonaws.com,1433;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=OitDb; " +
+                                       "connection timeout=30");
+            try
+            {
+                dbConnection.Open();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("SqlConnection Failed Exception" + e.Message);
+            }
+        }
+        ~DbConnection()
+        {
+            Console.WriteLine("Closing Sql Connection");
+            dbConnection.Close();
         }
     }
 }
