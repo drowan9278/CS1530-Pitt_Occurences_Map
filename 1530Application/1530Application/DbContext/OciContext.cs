6ab1a5fc1e80a80c4585b4d650626ec299967786
drowan9278 <drowan9278@students.mc3.edu>
2018-10-24 12:48:46 -0400

    Enity Connection

---
+++ 1530Application/1530Application/DbContext/OciContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _1530Application
+{
+    public class OciContext : DbContext
+    {
+        public OciContext()
+        {
+
+        }
+    }
+}
